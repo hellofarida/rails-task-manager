9518807f8539348dcb1e9b8f41be43b1f990972b
Farida Shebubakar <info@hellofarida.com>
2019-08-20 15:51:18 +1000

    Refactored form

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,9 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  append_after_action :redirect_to_task, only: [:create, :update]
+
   def index
     @tasks = Task.all
-  end
-
-  def show
-    @task = Task.find(params[:id])
   end
 
   def new
@@ -14,29 +13,28 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(tasks_param)
     @task.save
-
     redirect_to task_path(@task)
   end
 
-  def edit
-    @task = Task.find(params[:id])
-  end
-
   def update
-    @task = Task.find(params[:id])
     @task.update(tasks_param)
-
     redirect_to task_path(@task)
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
-
     redirect_to tasks_path
   end
 
   private
+
+  def redirect_to_task
+
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
 
   def tasks_param
     params.require(:task).permit(:title, :details, :completed)
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
-  get 'tasks', to: 'tasks#index', as: :tasks
-  get 'tasks/new', to: 'tasks#new', as: :new_task
-  post 'tasks', to: 'tasks#create', as: :create_task
+  resources :tasks
+  # Read (read one, read all), create (create and view), update (edit and show), destroy
 
-  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
-  patch 'tasks/:id', to: 'tasks#update', as: :update_task
+  # get 'tasks', to: 'tasks#index', as: :tasks
+  # post 'tasks', to: 'tasks#create'
 
-  delete 'tasks/:id', to: 'tasks#destroy'
+  # get 'tasks/new', to: 'tasks#new', as: :new_task
 
-  get 'tasks/:id', to: 'tasks#show', as: :task
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # get 'tasks/:id', to: 'tasks#show', as: :task
+
+  # patch 'tasks/:id', to: 'tasks#update', as: :update_task
+  # delete 'tasks/:id', to: 'tasks#destroy'
 end
